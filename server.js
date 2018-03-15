6bb38ad9eaf3ae3eff22c6ccc948bb40068dd4e1
jmshanahan <comeraghsolutions@gmail.com>
2018-03-15 11:40:38 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express();
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+app.get('/',(req,res) => {
+  res.send('hello js');
+});
+app.listen(4000, () => {
+  console.log('Listening');
+});
